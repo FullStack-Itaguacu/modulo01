76ae1ee77727fc0d8f1b77e54614b12e0755b8d9
Bruno Costa <brunocf.dev@hotmail.com>
2023-03-07 20:19:50 -0300

    first commit

---
+++ semana06/primeiro-projeto/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Card from "./components/Card"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ semana06/primeiro-projeto/src/components/Card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+  return ( 
+    <div>
+      <h1>Carro Ferrari Vermelho</h1>
+      <img width={500} src="https://s2.glbimg.com/WwkvfLhK4mt40-5fkdlWSHbyJqo=/0x0:1280x921/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/M/0/zLTv6fTfuWwIoCbUdZXA/ferrari-sf90-stradale-2020-1280-05.jpg" alt="Carro Ferrari" />
+    </div>
+   );
+}
+
+export default Card;
