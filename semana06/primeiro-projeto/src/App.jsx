552a7b244788dd5c2a6ce7d13eac4ff645468faf
Bruno Costa <brunocf.dev@hotmail.com>
2023-03-09 19:56:39 -0300

    m01s06 aula 02 - codigo final usado em sala

--- semana06/primeiro-projeto/src/App.jsx
+++ semana06/primeiro-projeto/src/App.jsx
@@ -3,9 +3,41 @@ import Card from "./components/Card"
 
 function App() {
 
+  const urlFerrari = "https://s2.glbimg.com/WwkvfLhK4mt40-5fkdlWSHbyJqo=/0x0:1280x921/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/M/0/zLTv6fTfuWwIoCbUdZXA/ferrari-sf90-stradale-2020-1280-05.jpg"
+
+  const urlAudi = "https://quatrorodas.abril.com.br/wp-content/uploads/2021/11/Audi-A3-Sedan-2.0-2022-1.jpg?quality=70&strip=info&w=1024&resize=1200,800"
+
+
+
+  const usuario = {
+    nome: "Bruno Costa",
+    idade: 24,
+    profissao: "Dev",
+    endereco: {
+      logradouro: "Sua rua",
+      numero: 45
+    }
+  }
+
+  const {nome, idade, profissao, endereco} = usuario
+
+  // const logradouro = usuario.endereco.logradouro
+
+  // const {logradouro} = endereco
+
+  nome
+  idade
+  profissao
+
+
+
+
+
   return (
     <div className="App">
-      <Card />
+      <img src="" alt="" />
+      <Card imagem={urlFerrari} width={300} alt="Imagem de um carro Ferrari"/>
+      <Card titulo="Carro Audi" imagem={urlAudi} width={500} alt="Imagem de um carro Audi" />
     </div>
   )
 }
--- semana06/primeiro-projeto/src/components/Card.jsx
+++ semana06/primeiro-projeto/src/components/Card.jsx
@@ -1,8 +1,20 @@
-function Card() {
+import "./card.css"
+
+function Card({titulo, imagem, width, alt}) {
+
+  /* 
+    var props = {
+      titulo: titulo,
+      imagem: imagem,
+      width: width
+    }
+  */
+
   return ( 
     <div>
-      <h1>Carro Ferrari Vermelho</h1>
-      <img width={500} src="https://s2.glbimg.com/WwkvfLhK4mt40-5fkdlWSHbyJqo=/0x0:1280x921/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/M/0/zLTv6fTfuWwIoCbUdZXA/ferrari-sf90-stradale-2020-1280-05.jpg" alt="Carro Ferrari" />
+      <h1 id="" className="titulo">{titulo}</h1>
+      <img width={width} src={imagem} alt={alt} />
+      <a href={imagem}>ver imagem</a>
     </div>
    );
 }
